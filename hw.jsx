a8ba30a279fe26603c531a601a7da1efe25d4fda
Sohail Siadat <sohale@gmail.com>
2017-08-04 23:06:05 +0100

    attempting to do GraphQL in this React

--- hw.jsx
+++ hw.jsx
@@ -44,22 +44,97 @@ ReactDOM.render(<App1 />, document.querySelector('#container'));
 // Warning: React.createElement: type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in.
 
 
-
+/*
 const ChannelsList = () =>
      (<ul>
        <li>Channel 1</li>
        <li>Channel 2</li>
      </ul>);
+*/
+
+
+
+
+
+
+// Prepare for GraphQL
+
+import {
+  ApolloClient,
+  gql,
+  graphql,
+  ApolloProvider,
+} from 'react-apollo';
+
+// const client = new ApolloClient();
+
+import { createNetworkInterface } from 'apollo-client';
+//import gql from 'graphql-tag';
+
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'https://api.github.com/graphql',
+  }),
+});
+// see https://github.com/apollographql/apollo-client
+// see https://developer.github.com/v4/guides/forming-calls/#the-graphql-endpoint
+
+
+/*const channelsListQuery = gql`
+   query ChannelsListQuery {
+     channels {
+       id
+       name
+     }
+   }
+ `;
+ */
+
+const channelsListQuery = gql`
+   query ChannelsListQuery {
+     channels {
+       id
+       name
+     }
+   }
+ `;
+
+
+
+const ChannelsList = ({ data: {loading, error, channels }}) => {
+   if (loading) {
+     return <p>Loading ...</p>;
+   }
+   if (error) {
+     return <p>{error.message}</p>;
+   }
+
+   return <ul>
+     { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
+   </ul>;
+ };
+
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
+
+
+
 
 
 class App2 extends React.Component {
    render() {
      return (
+                                                <ApolloProvider client={client}>
        <div className="ok">
-         <ChannelsList />
+         <ChannelsListWithData />
        </div>
+                                                </ApolloProvider>
      );
    }
  }
 
 ReactDOM.render(<App2 />, document.querySelector('#app2div'));
+
+
+
+
